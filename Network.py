d30acd03d2ef30cf33ac8f9ab9502310d5a00571
Nicolas Arias <jnas200071@outlook.com>
2024-03-10 11:34:38 -0500

    Modificacion final

--- Network.py
+++ Network.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from tabulate import tabulate
 
 # Se carga la infomacion del archivo excel
 # dr = pd.read_excel('Table 1.xlsx',sheet_name='player_code_name',index_col=0) 
@@ -12,8 +13,8 @@ G = nx.from_pandas_adjacency(df)
 # R = nx.from_pandas_edgelist(dr, source = 'NODE NUMBER', target = 'PLAYER NAME', )
 
 # Se separa los nodos por equipo
-e1 = list(G.nodes())[1:14]
-e2 = list(G.nodes())[15:G.number_of_nodes()]
+e1 = list(G.nodes())[0:14]
+e2 = list(G.nodes())[14:G.number_of_nodes()]
 
 se1 = G.subgraph(e1)
 se2 = G.subgraph(e2)
@@ -43,9 +44,6 @@ print ("Número de nodos:",num_nodos)
 print ("- - - - - - - - - - - - - - -")
 print ("Número de bordes:",num_bordes)
 print ("- - - - - - - - - - - - - - -")
-for i, promedio in avedegree.items():
-     print (f"Average Degree {i}",{promedio})
-print ("- - - - - - - - - - - - - - -")
 print ("Average Degree Global",avedegreeG)
 print ("- - - - - - - - - - - - - - -")
 print ("Average Path Length",avepathlength)
@@ -59,7 +57,9 @@ print ("- - - - - - - - - - - - - - -")
 print ("Radius",radius)
 print ("+ + + + + + + + + + + + + + +")
 
-print()
+print ("- - - - - - - - - - - - - - -")
+for i, promedio in avedegree.items():
+     print (f"Average Degree {i}",{promedio})
 
 # Equipo 2
 print ("+ + + + + + + + + + + + + + +")
@@ -79,9 +79,6 @@ print ("Número de nodos:",num_nodos)
 print ("- - - - - - - - - - - - - - -")
 print ("Número de bordes:",num_bordes)
 print ("- - - - - - - - - - - - - - -")
-for i, promedio in avedegree.items():
-     print (f"Average Degree {i}",{promedio})
-print ("- - - - - - - - - - - - - - -")
 print ("Average Degree Global",avedegreeG)
 print ("- - - - - - - - - - - - - - -")
 print ("Average Path Length",avepathlength)
@@ -95,41 +92,76 @@ print ("- - - - - - - - - - - - - - -")
 print ("Radius",radius)
 print ("+ + + + + + + + + + + + + + +")
 
+for i, promedio in avedegree.items():
+     print (f"Average Degree {i}",{promedio})
 
 # Node Calulos
 #Equipos #1
 print("Equipo #1")
-print ("- - - - - - - - - - - - - - -")
-print("Node    | Closeness ")
-print ("- - - - - - - - - - - - - - -")
 Closeness = nx.closeness_centrality(se1)
-for i, valor in Closeness.items(): 
-    print(f"Node {i} | {valor}")
-    print ("- - - - - - - - - - - - - - -")
-print ("- - - - - - - - - - - - - - -")
-print("Node    | Pagerank ")
-print ("- - - - - - - - - - - - - - -")
 Pagerank = nx.pagerank(se1)
-for i, valor in Pagerank.items(): 
-    print(f"Nod{i}   | {valor}")
-    print ("- - - - - - - - - - - - - - -")
-
-print ("- - - - - - - - - - - - - - -")
-print("Node    | Betweenness ")
-print ("- - - - - - - - - - - - - - -")
 betweenness = nx.betweenness_centrality(se1)
-for i, valor in betweenness.items():
-    print(f"Node {i} | {valor}")
-    print ("- - - - - - - - - - - - - - -")
+data = []
+for node in se1.nodes():
+     data.append([node,Closeness[node],Pagerank[node],betweenness[node]])
+data_sorted = sorted(data,key=lambda x: x[0])
+headers = ["Nodo", "Closeness Centrality", "PageRank","Betweenness"]
+print(tabulate(data_sorted, headers=headers, tablefmt="grid"))
+
+pos = nx.spring_layout(se1)
+plt.figure()
+plt.title("Network Equipo#1")
+nx.draw(se1,pos, with_labels=True, node_color=range(14),alpha=0.8)
+plt.figure()
+plt.title("Betweeness Equipo#1")
+nx.draw(se1,pos, labels={node: f"{betweenness[node]:.2f}" for node in se1.nodes()}, node_color=range(14), node_size= [100000*betweenness[node] for node in se1.nodes()],alpha=0.8)
+plt.figure()
+plt.title("Closeness Equipo#1")
+nx.draw(se1,pos, labels={node: f"{Closeness[node]:.2f}" for node in se1.nodes()}, node_color=range(14), node_size= [1000*Closeness[node] for node in se1.nodes()],alpha=0.8)
+
+#Equipos #2
+print("Equipo #2")
+Closeness = nx.closeness_centrality(se2)
+Pagerank = nx.pagerank(se2)
+betweenness = nx.betweenness_centrality(se2)
+data = []
+for node in se2.nodes():
+     data.append([node,Closeness[node],Pagerank[node],betweenness[node]])
+data_sorted = sorted(data,key=lambda x: x[0])
+headers = ["Nodo", "Closeness Centrality", "PageRank","Betweenness"]
+print(tabulate(data_sorted, headers=headers, tablefmt="grid"))
 
 
+# for i, valor in Closeness.items(): 
+#     print(f"Node {i} | {valor}")
+#     print ("- - - - - - - - - - - - - - -")
+# print ("- - - - - - - - - - - - - - -")
+# print("Node    | Pagerank ")
+# print ("- - - - - - - - - - - - - - -")
 
-plt.figure(figsize=(8, 4))
-plt.subplot(1, 3, 1)
-nx.draw(se1, with_labels=True)
-plt.subplot(1, 3, 2)
-nx.draw(se1, with_labels=True, node_color=range(13), node_size= [10000*betweenness[node] for node in se1.nodes()])
-plt.subplot(1, 3, 3)
-nx.draw(se1, with_labels=True, node_color=range(13), node_size= [1000*Closeness[node] for node in se1.nodes()])
+# for i, valor in Pagerank.items(): 
+#     print(f"Node {i}  | {valor}")
+#     print ("- - - - - - - - - - - - - - -")
+
+# print ("- - - - - - - - - - - - - - -")
+# print("Node    | Betweenness ")
+# print ("- - - - - - - - - - - - - - -")
+
+# for i, valor in betweenness.items():
+#     print(f"Node {i} | {valor}")
+#     print ("- - - - - - - - - - - - - - -")
+
+
+pos = nx.spring_layout(se2)
+plt.figure()
+plt.title("Network Equipo#2")
+nx.draw(se2,pos, with_labels=True, node_color=range(14),alpha=0.8)
+plt.figure()
+plt.title("Betweeness Equipo#2")
+nx.draw(se2,pos, labels={node: f"{betweenness[node]:.2f}" for node in se2.nodes()}, node_color=range(14), node_size= [100000*betweenness[node] for node in se2.nodes()],alpha=0.8)
+plt.figure()
+plt.title("Closeness Equipo#2")
+nx.draw(se2,pos, labels={node: f"{Closeness[node]:.2f}" for node in se2.nodes()}, node_color=range(14), node_size= [1000*Closeness[node] for node in se2.nodes()],alpha=0.8)
+
 
 plt.show()
